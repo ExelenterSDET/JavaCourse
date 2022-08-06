165c844292bec4fb373a50ad875617aae54ea94f
andreamikulin <andreamikulin@gmail.com>
2022-08-06 11:02:54 -0600

    Andrea is trying this again

--- src/Day1/Practice/Practice.java
+++ src/Day1/Practice/Practice.java
@@ -5,6 +5,7 @@ import java.sql.SQLOutput;
 public class Practice {
     public static void main(String[] args) {
 
+        System.out.println("Help Andrea");
         System.out.println("Pushing from Said feature");
         
         System.out.println("test");
