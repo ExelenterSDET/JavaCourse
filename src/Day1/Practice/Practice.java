6f1fdf1c9129faf2a9146adf8f36fc6bba940dfb
Furkat-Ali-Khalilov <khalilov.furkat.ali@gmail.com>
2022-08-05 22:16:13 -0400

    Furkat Ali added

--- src/Day1/Practice/Practice.java
+++ src/Day1/Practice/Practice.java
@@ -5,6 +5,7 @@ public class Practice {
 
         System.out.println("Pushing from Said feature");
         
-        System.out.println("test")
+        System.out.println("test");
+        System.out.println("Furkat Ali");
     }
 }
