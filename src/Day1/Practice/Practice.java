1f90379029a07641956d522762781fb9c942d2be
bahodurotayev <tjkbaha@gmail.com>
2022-08-05 22:38:34 -0400

    BO commit

--- src/Day1/Practice/Practice.java
+++ src/Day1/Practice/Practice.java
@@ -3,5 +3,7 @@ package Day1.Practice;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("BO");
+        System.out.println("BO");
     }
 }
