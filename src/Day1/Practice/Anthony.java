3adfbc7c56ac510f5e4cc31a13a34262b4f72ba2
AnthonyHermann182 <herrmanthony2014@yahoo.com>
2022-08-08 20:27:10 -0700

    Practice Escape Characters AnthonyH.

--- src/Day1/Practice/Anthony.java
+++ src/Day1/Practice/Anthony.java
@@ -1,5 +1,4 @@
 package Day1.Practice;
-
 public class Anthony {
     public static void main(String[] args) {
         //Practice Escape Characters
@@ -10,9 +9,8 @@ public class Anthony {
         System.out.println("\"My name is AnthonyHermann\"");
         System.out.println("My name is " + "Anthony" + " I am 36");
         // System.out.println("\"Coding \nis \npretty \nrough\"");
-        System.out.println("\"This \tCoding \tClass \nis \nreally \nworking\"");
+        System.out.println("\"This \tCoding \tClass is really working\"");
         //googled escape charater in java for practice
-
         System.out.println("What happens\rwhen we use a carriage?");
         //prints everything after \r
         System.out.println("\fwhat is a form feed\f");
@@ -21,7 +19,5 @@ public class Anthony {
         System.out.println("\"Lets try a double quote character\"");
         System.out.println("I went to the beach and there was a lady.\nShe was selling sea shells by the Seashore!");
         System.out.println("She replied,\"Hello!- would you like a seashell?\"");
-        System.out.println("Intrigued I replied,\"How much would a seashell cost?\"");
-
-    }
+        System.out.println("Intrigued I replied,\"How much would a seashell cost?\"");}
 }
