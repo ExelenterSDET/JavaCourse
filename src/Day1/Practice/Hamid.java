76b0f9dd8bf534c18ae764eb6f91ffc627b420c9
sharifzodah <sharifzodah@gmail.com>
2022-08-09 19:36:21 -0400

    Escape chars hw Hamid

--- src/Day1/Practice/Hamid.java
+++ src/Day1/Practice/Hamid.java
@@ -2,8 +2,23 @@ package Day1.Practice;
 
 public class Hamid {
 
-    public static void main(String[] args) {
 
-        System.out.println("Practice");
+    public static void main (String [] args){
+
+
+        byte num = 127;  // range of the byte is -128  to  127
+        byte num2 = -128;
+        byte total = (byte) (num + num2);
+
+        boolean b = num == num2;
+
+        System.out.println(total);
+
+        System.out.println(" \"Hello world\"\t Hi \\ ");
+
+
+
+
     }
+
 }
