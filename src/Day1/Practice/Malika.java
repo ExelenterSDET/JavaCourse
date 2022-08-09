f7404adf73362ad5958d9e1c6316a2b19d157be5
malikasadykova <Malika.sadykova@yahoo.com>
2022-08-10 02:05:42 +0300

    malika practice character day1

--- src/Day1/Practice/Malika.java
+++ src/Day1/Practice/Malika.java
@@ -1,7 +1,40 @@
 package Day1.Practice;
 
+import Day1.Java.JavaIntro;
+
 public class Malika {
     public static void main(String[] args) {
-        
+        // JavaIntro
+        System.out.println("Hello World");
+        System.out.println(2022);
+        System.out.println("My name is " + "Malika " + "I am " + 30);
+        System.out.println("My name is Malika I am " + 30);
+        System.out.println("My name is Malika I am 30");
+        // EscapeCharacters
+        System.out.println("\"Hello World\"");
+        System.out.println("Malika \nSadykova");
+        System.out.println("\\Disney\\\t\\Land\\");
+        //Byte
+        byte min = -128;
+        byte max = 127;
+        byte number1 = 100;
+        byte number2 = 50;
+        byte total = (byte) (number1 + number2);
+        System.out.println("total = " + total);
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+        //Boolean
+        boolean bl = true;
+        boolean bl1 = false;
+        boolean bl3 = (2==2);
+        boolean bl4 = (150==200);
+        boolean bl5 = (240*2 == 1440/3);
+        System.out.println(bl3);
+        System.out.println(bl4);
+        System.out.println(bl5);
+
+
+
+
     }
 }
