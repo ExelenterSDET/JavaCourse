d78fabec0585c6f204ed412101a418e810d9b9f2
AnthonyHermann182 <herrmanthony2014@yahoo.com>
2022-08-08 19:49:40 -0700

    Practicing Escape Characters AnthonyH

--- src/Day1/Practice/Anthony.java
+++ src/Day1/Practice/Anthony.java
@@ -3,5 +3,14 @@ package Day1.Practice;
 public class Anthony {
     public static void main(String[] args) {
         //Practice Escape Characters
+        //System.out.println("\"Hello World\"");
+        //System.out.println("Anthony \nHermann");
+        //System.out.println("\\Disney\\\t\\Land\\");
+        //System.out.println(" \\ ");
+        System.out.println("\"My name is AnthonyHermann\"");
+        System.out.println("My name is " + "Anthony" + " I am 36");
+        // System.out.println("\"Coding \nis \npretty \nrough\"");
+        System.out.println("\"This \tCoding \tClass \nis \nreally \nworking\"");
+
     }
 }
