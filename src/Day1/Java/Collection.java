8a95eddd35ea1cc14f20854b9a49367a80439492
sebahattinbeycur <beycur@gmail.com>
2022-08-09 19:38:28 +0300

    Collection

---
+++ src/Day1/Java/Collection.java
@@ -0,0 +1,10 @@
+package Day1.Java;
+
+public class Collection {
+    public static void main(String[] args) {
+            double sonuc=(((9.5*4.5)-(2.5*3)) / (45.5-3.5));
+            int as= (int)(sonuc*10);
+            System.out.println("Sonuc="+ sonuc);
+            System.out.println("Sonuc="+ as);
+    }
+}
