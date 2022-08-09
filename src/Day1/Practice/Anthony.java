b25be242a32e6bc6d0f01d39bf6ef9619182a5e4
AnthonyHermann182 <herrmanthony2014@yahoo.com>
2022-08-08 20:10:30 -0700

    added a few more escape characters

--- src/Day1/Practice/Anthony.java
+++ src/Day1/Practice/Anthony.java
@@ -11,6 +11,17 @@ public class Anthony {
         System.out.println("My name is " + "Anthony" + " I am 36");
         // System.out.println("\"Coding \nis \npretty \nrough\"");
         System.out.println("\"This \tCoding \tClass \nis \nreally \nworking\"");
+        //googled escape charater in java for practice
+
+        System.out.println("What happens\rwhen we use a carriage?");
+        //prints everything after \r
+        System.out.println("\fwhat is a form feed\f");
+        System.out.println("\fboxed out\f");
+        System.out.println("\'I will use this single quote character\'");
+        System.out.println("\"Lets try a double quote character\"");
+        System.out.println("I went to the beach and there was a lady.\nShe was selling sea shells by the Seashore!");
+        System.out.println("She replied,\"Hello!- would you like a seashell?\"");
+        System.out.println("Intrigued I replied,\"How much would a seashell cost?\"");
 
     }
 }
