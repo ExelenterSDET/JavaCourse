d3a274e3a66be744a02d177eb65caaab0c49247d
AnthonyHermann182 <herrmanthony2014@yahoo.com>
2022-08-09 19:24:29 -0700

    Char Homework Anthony

---
+++ src/Day2/Homework/CharHomeworkAnthony.java
@@ -0,0 +1,44 @@
+package Day2.Homework;
+
+public class CharHomeworkAnthony {
+    /*
+create char variables and assign values as each character of your name
+*/
+    static char defaultValue;
+    //default value of char is null box looking thing
+
+    public static void main (String[] args) {
+
+        char A = 65;
+        char N = 78;
+        char T = 84;
+        char H = 72;
+        char O = 79;
+        char Y = 89;
+        char space = ' ';
+        //char space = ' '; space in between
+        //H
+        char E = 69;
+        char R = 82;
+        char M = 77;
+        //A
+        //N
+        //N
+
+        System.out.print(A);
+        System.out.print(N);
+        System.out.print(T);
+        System.out.print(H);
+        System.out.print(O);
+        System.out.print(N);
+        System.out.print(Y);
+        System.out.print(' ');
+        System.out.print(H);
+        System.out.print(E);
+        System.out.print(R);
+        System.out.print(M);
+        System.out.print(A);
+        System.out.print(N);
+        System.out.print(N);
+    }
+}
