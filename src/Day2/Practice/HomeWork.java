c7dede8a81a821308b38d56269f94ee42ccaf4d6
Bakhridin <bakhridin76@gmail.com>
2022-08-09 19:34:09 -0700

    Char manipulation

---
+++ src/Day2/Practice/HomeWork.java
@@ -0,0 +1,30 @@
+package Day2.Practice;
+
+public class HomeWork {
+    public static void main(String[] args) {
+
+        char b1= 'B';
+        char a1='A';
+        char k1 ='K';
+        char h1='H';
+        char r1='R';
+        char i1='I';
+        char d1='D';
+        char i2='I';
+        char n1='N';
+
+        System.out.println(b1+""+a1+""+k1+""+h1+""+r1+""+i1+""+d1+""+i2+""+n1);
+
+        char B=66;
+        char A=65;
+        char K=75;
+        char H=72;
+        char R=82;
+        char I=73;
+        char D=68;
+        char I1=73;
+        char N=78;
+        System.out.println(B+""+A+""+K+""+H+""+R+""+I+""+D+""+I1+""+N);
+
+    }
+}
