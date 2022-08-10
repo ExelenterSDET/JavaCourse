fcec5fbc5b983c6ae2e1a3f11bece8fcde4a3dae
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-08-09 22:43:50 -0400

    DilnozaHW

---
+++ src/Day2/Practice/Dilnoza.java
@@ -0,0 +1,22 @@
+package Day2.Practice;
+
+public class Dilnoza {
+    public static void main(String[] args) {
+        char D = 68;
+        char i =105;
+        char l = 108;
+        char n =110 ;
+        char o = 111;
+        char z =122;
+        char a = 97;
+
+        System.out.print( D);
+        System.out.print(i);
+        System.out.print( l);
+        System.out.print( n);
+        System.out.print( o);
+        System.out.print( z);
+        System.out.print(a);
+
+    }
+}
