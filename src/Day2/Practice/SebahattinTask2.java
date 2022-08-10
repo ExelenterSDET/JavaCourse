73591c9c4a013bc535c7fb57426cf3aaf79f4744
sebahattinbeycur <beycur@gmail.com>
2022-08-10 12:24:49 +0300

    Added 3 track to practice

---
+++ src/Day2/Practice/SebahattinTask2.java
@@ -0,0 +1,22 @@
+package Day2.Practice;
+
+public class SebahattinTask2 {
+    public static void main(String[] args) {
+
+        double watermelon = 0.99;
+        double strawberry = 4.20;
+        double milk = 6.99;
+        float cake = 2.55f;
+        float banana = 4.9f;
+        float shampoo = 8.99f;
+
+
+        System.out.println("watermelon = $" + watermelon);
+        System.out.println("strawberry = $" + strawberry);
+        System.out.println("milk = $" + milk);
+        System.out.println("cake = $" + cake);
+        System.out.println("banana = $" + banana);
+        System.out.println("shampoo = $" + shampoo);
+
+    }
+}
