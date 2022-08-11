02f4311c0f066abd8f59f0edb17f3aa1a924da24
sebahattinbeycur <beycur@gmail.com>
2022-08-11 04:51:16 +0300

    Java Reasing Value added

---
+++ src/Day2/Practice/JavaReasignValue.java
@@ -0,0 +1,39 @@
+package Day2.Practice;
+
+public class JavaReasignValue {
+    public static void main(String[] args) {
+
+        int num1 = 100;
+        System.out.println("num1 = "+num1);
+        num1 = 200;
+        System.out.println("num1 = "+num1);
+        num1 = 300;
+        System.out.println("num1 = "+num1);
+        num1 = 400;
+        System.out.println("num1 = "+num1);
+        num1 = 500;
+        System.out.println("num1 = "+num1);
+
+        char ch = 'S';
+        System.out.println(ch);
+        ch = 'E';
+        System.out.println(ch);
+        ch = 'B';
+        System.out.println(ch);
+        ch = 'A';
+        System.out.println(ch);
+        ch = 'H';
+        System.out.println(ch);
+        ch = 'A';
+        System.out.println(ch);
+        ch = 'T';
+        System.out.println(ch);
+        ch = 'T';
+        System.out.println(ch);
+        ch = 'I';
+        System.out.println(ch);
+        ch = 'N';
+        System.out.println(ch);
+
+    }
+}
