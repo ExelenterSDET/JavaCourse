7ec5aba1944f7353d54b4dffddd1acaafa668e3b
AnthonyHermann182 <herrmanthony2014@yahoo.com>
2022-08-10 21:56:13 -0700

    AnthonyH Day2 HW

---
+++ src/Day2/Practice/AnthonyH.java
@@ -0,0 +1,43 @@
+package Day2.Practice;
+
+public class AnthonyH {
+    /*create char variables and assign values as each character of your name
+    */
+    //static char defaultValue;
+    //default value of char is null box looking thing
+
+    public static void main (String[] args) {
+
+        char A = 65;
+        char N = 78;
+        char T = 84;
+        char H = 72;
+        char O = 79;
+        char Y = 89;
+        char space = ' ';
+        //char space = ' '; space in between
+        //H
+        char E = 69;
+        char R = 82;
+        char M = 77;
+        //A
+        //N
+        //N
+
+        System.out.print(A);
+        System.out.print(N);
+        System.out.print(T);
+        System.out.print(H);
+        System.out.print(O);
+        System.out.print(N);
+        System.out.print(Y);
+        System.out.print(space);
+        System.out.print(H);
+        System.out.print(E);
+        System.out.print(R);
+        System.out.print(M);
+        System.out.print(A);
+        System.out.print(N);
+        System.out.print(N);
+    }
+}
