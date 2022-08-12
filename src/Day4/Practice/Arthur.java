44f3d88c1ad9b3fd19a42b4e54e9662c2035f761
bennew11 <bennew11@github.com>
2022-08-11 22:41:18 -0400

    arthur day 4 tasks

---
+++ src/Day4/Practice/Arthur.java
@@ -0,0 +1,45 @@
+package Day4.Practice;
+
+public class Arthur {
+    public static void main(String[] args) {
+        byte father = 80, brother = 70, sister = 25;
+
+        System.out.println(father);
+        System.out.println(brother);
+        System.out.println(sister);
+        System.out.println(father);
+
+        //        int num = 10;
+//        int num2 = 10;
+//        num = num + 5;
+//        System.out.println("num = " + num);
+//        num2 += 5;
+//        System.out.println("num2 = " + num2);
+//        num2 *= 5;
+//        System.out.println("num2 = " + num2);
+//        num2 /= 5;
+
+        double num = 50;
+        System.out.println("num2 = " + num);
+
+        num += 70;
+        System.out.println("num2 = " + num);
+
+        num /= 4;
+        System.out.println("num2 = " + num);
+
+        num %= 6;
+        System.out.println("num2 = " + num);
+
+        num *= 6;
+        System.out.println("num2 = " + num);
+
+        num -= 49;
+        System.out.println("num2 = " + num);
+
+
+
+
+
+    }
+}
