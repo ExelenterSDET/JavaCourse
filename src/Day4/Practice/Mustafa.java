0e05e35c66427618aa7bcbe289038fcc5bb968f4
MustafaIsmailKardzhi <munesoo01@gmail.com>
2022-08-12 05:23:32 +0300

    day4 Mustafa

---
+++ src/Day4/Practice/Mustafa.java
@@ -0,0 +1,25 @@
+package Day4.Practice;
+
+public class Mustafa {
+    public static void main(String[] args) {
+        byte gulnaz = 43, ahmet = 48, esma = 23, bushra = 19, Esat = 14;
+        System.out.println("gulnaz = " + gulnaz);
+        System.out.println("ahmet = " + ahmet);
+        System.out.println("esma = " + esma);
+        System.out.println("bushra = " + bushra);
+        System.out.println("Esat = " + Esat);
+
+        int int1 = 50;
+        int1 += 70;
+        System.out.println("int1 = " + int1);
+        int1 /=4;
+        System.out.println("int1 = " + int1);
+        int1 %=7;
+        System.out.println("int1 = " + int1);
+        int1 *=10;
+        System.out.println("int1 = " + int1);
+        int1 -=49;
+        System.out.println("int1 = " + int1);
+
+    }
+}
