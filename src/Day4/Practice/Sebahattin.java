b97e6d839513e7c4eae7cac17bd42bb1e330e02b
sebahattinbeycur <beycur@gmail.com>
2022-08-12 04:33:40 +0300

    Java assigment operators

---
+++ src/Day4/Practice/Sebahattin.java
@@ -0,0 +1,15 @@
+package Day3.Practice;
+
+public class Sebahattin {
+    public static void main(String[] args) {
+
+        byte ft = 64,mt = 62, br= 37, wf= 40, ms=42 ;
+
+        System.out.println("father = " + ft );
+        System.out.println("mother = " + mt );
+        System.out.println("brother = " + br );
+        System.out.println("my wife = " + wf );
+        System.out.println("myself= " + ms );
+
+    }
+}
