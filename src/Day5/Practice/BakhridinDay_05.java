e3aa47484727b6cfd5043eabee9ba846e264b628
Bakhridin <bakhridin76@gmail.com>
2022-08-12 20:13:21 -0700

    Day 5 Home Work

---
+++ src/Day5/Practice/BakhridinDay_05.java
@@ -0,0 +1,35 @@
+package Day5.Practice;
+
+public class BakhridinDay_05 {
+    public static void main(String[] args) {
+
+        // Task for homework.
+        // calculate total of number 563. (dont use hardcoding) answer should be 14;
+
+        int number1=563;
+        int divider1=10;
+        int totalOfReminders=0;
+
+        //Step1
+        //number1/=divider1;
+        System.out.println("number1 = " + number1);
+        int reminder1=number1%divider1;
+        System.out.println("reminder1 = " + reminder1);
+        totalOfReminders=number1%divider1;
+        System.out.println("totalOfReminders = " + totalOfReminders);
+        //Step2
+        number1/=divider1;
+        System.out.println("number1 = " + number1);
+        reminder1=number1%divider1;
+        System.out.println("reminder1 = " + reminder1);
+        totalOfReminders+=reminder1;
+        System.out.println("totalOfReminders = " + totalOfReminders);
+        //Step3
+        number1/=divider1;
+        System.out.println("number1 = " + number1);
+        reminder1=number1%divider1;
+        System.out.println("reminder1 = " + reminder1);
+        totalOfReminders+=reminder1;
+        System.out.println("totalOfReminders = " + totalOfReminders);
+    }
+}
