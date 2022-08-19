f2d106341ad10974e0090785f3ed619a7098926d
SaafarST <tohir.s91@gmail.com>
2022-08-18 17:55:02 -0700

    added new comment line

--- src/Day8/Practice/Tohir.java
+++ src/Day8/Practice/Tohir.java
@@ -3,5 +3,6 @@ package Day8.Practice;
 public class Tohir {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World!!!");
     }
 }
