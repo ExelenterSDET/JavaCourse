3b5e51835b57a6ff6965fa491c43247984b15a02
SaafarST <tohir.s91@gmail.com>
2022-08-21 13:10:27 -0700

    5, 8 and 14 is left unsolved

--- src/Day10/Practice/Tohir.java
+++ src/Day10/Practice/Tohir.java
@@ -202,5 +202,76 @@ public class Tohir {
     }
 
      */
-    
+    //Task 12 Create two strings. Check if the strings contain any upper case letters.
+    //If strings contain capital letters, return the total count of capital letters for both strings.
+    //if, one of the strings only contains capital letters, return the count of all characters for that string.
+    //if none of the string contain capital letters, return 0
+    //Example
+    //Hello and World ==> output should be 2
+    //Hello and world ==> output should be 5
+    //hello and world ==> output should be 10
+    /*
+    public static void main(String[] args) {
+        Scanner Str = new Scanner(System.in);
+        String str1 = Str.next();
+        String str2 = Str.next();
+        Boolean case_1 = str1.matches(".*[A-Z].*") && str2.matches(".*[A-Z].*");
+        Boolean case_2 = ((str1.matches(".*[A-Z].*") && !str2.matches(".*[A-Z].*"))||(!str1.matches(".*[A-Z].*") && str2.matches(".*[A-Z].*")));
+        Boolean case_3 = !(case_1);
+
+        if (case_1) {
+            int num = str1.replaceAll("[a-z]", "").length()+str2.replaceAll("[a-z]", "").length();
+            System.out.print("Total " + num );
+        }
+        if (case_2) {
+            int num = str1.replaceAll("[a-z]", "").length();//+str2.replaceAll("[a-z]", "").length();
+            System.out.print("Total " + num );
+        }
+        if (case_3) {
+            System.out.print("0");
+        }
+    }
+
+     */
+
+    //Task #13
+    //Create a string and a number. Convert number to char.
+    //if string contains char, return the character it self.
+    //if string doesnt contain char, return "Character doesnt exist in the string"
+    /*
+    public static void main(String[] args) {
+        Scanner Str = new Scanner(System.in);
+        String str1 = Str.next();
+        int str2 = Str.nextInt();
+        char c = (char) str2;
+        if (str1.contains(String.valueOf(c))){
+            System.out.print(str1);
+        }
+        else {
+            System.out.print("Character doesnt exist in the string");
+        }
+*/
+
+    //Task 14
+    //Create a string. Validate if a string has a space. (use maximum 1 spaces in the string)
+    //if string doesnt have a space, return the string itself.
+    //if string has a space, get two words to the left from the space and to the right from the space. validate which word is the longest and print it.
+    //if words have equal length, return the whole string.
+    //Example:
+    //Hello World = Hello = 5, World = 5 Output should be "Hello World"
+    //Exelenter Sdet = Exelenter = 9, Sdet = 4 Ouput should be "Exelenter"
+    public static void main(String[] args) {
+        Scanner Str = new Scanner(System.in);
+        String str1 = Str.next();
+        //Boolean bl1 = str1.contains(" ");
+        Boolean bl2 = str1.matches(".*\\w.*");
+        if(bl2) {
+            int n = str1.indexOf(" ");
+        }
+        if(!bl2){
+            System.out.print(str1);
+        }
+
+    }
+
 }
