a8eb581a153b32f93faf32d511114f7e6b1cdeb2
SaafarST <tohir.s91@gmail.com>
2022-08-24 09:06:34 -0700

    Tohir Day 5

---
+++ src/Day6/Practice/Tohir.java
@@ -0,0 +1,14 @@
+package Day6.Practice;
+import java.util.Scanner;
+public class Tohir {
+
+   public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       String  str = scan.nextLine();
+    //Concatenation method .concat(string);
+    System.out.println(str.concat(" Hello"));
+    // . lenght(string) method
+       System.out.println("Lenght of string + hello is " +str.concat("Hello").length());
+   }
+
+}
