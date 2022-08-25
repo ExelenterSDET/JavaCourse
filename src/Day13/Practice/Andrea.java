32b5d73ab3370631a7a2e2b1459b40fe6524673f
Andrea Mikulin <andreamikulin@gmail.com>
2022-08-25 17:42:32 -0600

    Andrea fixed a problem

--- src/Day13/Practice/Andrea.java
+++ src/Day13/Practice/Andrea.java
@@ -12,9 +12,9 @@ public class Andrea {
 
             String output = "";
 
-            output = password.length() >= 8 && password.replaceAll("[A-Z]","").length() > 0
-                    && password.replaceAll("[a-z]","").length() > 0
-                    && password.replaceAll("[A-Za-z0-9]", "").length() > 0 ? "Strong password" : "Invalid password";
+            output = password.length() >= 8 && password.replaceAll("[^A-Z]","").length() > 0
+                    && password.replaceAll("[^a-z]","").length() > 0
+                    && password.replaceAll("[^A-Za-z0-9]", "").length() > 0 ? "Strong password" : "Invalid password";
 
             System.out.println(output);
 
