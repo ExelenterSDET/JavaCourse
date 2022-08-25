2871474e864f4ea7349b96e82102183b3f09f39d
mehmet <atesnrg@gmail.com>
2022-08-25 18:39:37 -0500

    8/25/22

--- src/Day13/java/TaskPasswordChecker.java
+++ src/Day13/java/TaskPasswordChecker.java
@@ -1,5 +1,7 @@
 package Day13.java;
 
+import java.util.Scanner;
+
 public class TaskPasswordChecker {
     public static void main(String[] args) {
         /* Task password checker
@@ -12,7 +14,29 @@ public class TaskPasswordChecker {
         if password meets requirement return 'Strong password' else return "Invalid password"
          */
 
+        System.out.println("Please enter your password");
+        Scanner scan = new Scanner(System.in);
+        String pass = scan.nextLine();
+        if (pass.length() >= 7) {
+            if (!pass.toLowerCase().equals(pass)) {
+                if (!pass.toUpperCase().equals(pass)) {
+                    if (pass.contains("*") || pass.contains("+") || pass.contains("-") || pass.contains(":") || pass.contains("$") || pass.contains("#")){
+                        System.out.println("you are log in");
+                    }
+                    else {
+                        System.out.println("min 1 special character (* + - : $ #)");
+                    }
+                }
+                else {
+                    System.out.println("min 1 lower case letter");
+                }
+            } else {
+                System.out.println("min 1 Capital letter");
+            }
+        } else {
+            System.out.println("min 8 number");
 
 
+        }
     }
 }
