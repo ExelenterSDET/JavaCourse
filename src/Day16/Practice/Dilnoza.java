dad10a0a18a554444724ebd2f11962c13e7277fc
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-08-29 22:42:04 -0400

    Task3 & Task5

--- src/Day16/Practice/Dilnoza.java
+++ src/Day16/Practice/Dilnoza.java
@@ -9,7 +9,7 @@ public class Dilnoza {
                 Return reverse of a string.
                 Example: str = Java, result should be = avaJ
                 */
-
+        System.out.println("===============TASK1================");
         String str = "Java";
         String newStr ="";
         for (int i = str.length()-1; i >=0 ; i--) {
@@ -17,5 +17,31 @@ public class Dilnoza {
             newStr+=str.charAt(i);
         }
         System.out.println(newStr);
+
+        System.out.println("===============TASK2================");
+
+
+
+        System.out.println("===============TASK2================");
+        int number=2;
+
+        for (int i = 1; i < 10; i++){
+            int number2 = number*i;
+            System.out.println(number+"*" +i +"=" + number2);
+        }
+
+        System.out.println("===============TASK5================");
+        int count = 0;
+
+        for (int i = 8; i < 100; i+=8) {
+            count ++;
+            System.out.println("count = " + count);
+        }
+
+
+
+
+        
     }
+
 }
