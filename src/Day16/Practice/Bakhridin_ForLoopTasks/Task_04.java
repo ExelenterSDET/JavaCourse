44b52cc7cb78da9ba95fffb70c644d239fc81e61
bakhridin <bakhridin76@gmail.com>
2022-08-29 19:58:22 -0700

    Day 16 Tasks

---
+++ src/Day16/Practice/Bakhridin_ForLoopTasks/Task_04.java
@@ -0,0 +1,13 @@
+package Day16.Practice.Bakhridin_ForLoopTasks;
+
+public class Task_04 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=10; i+=2) {
+            int j=i+1;
+            System.out.println(i+" "+j);
+
+        }
+    }
+
+}
