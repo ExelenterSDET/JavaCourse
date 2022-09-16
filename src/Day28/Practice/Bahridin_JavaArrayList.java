b9872004d0c637c195d20a5feebafac7d4ae0d62
bakhridin <bakhridin76@gmail.com>
2022-09-16 16:13:34 -0700

    Bahridin_ArrayList

---
+++ src/Day28/Practice/Bahridin_JavaArrayList.java
@@ -0,0 +1,29 @@
+package Day28.Practice;
+
+import java.util.ArrayList;
+
+public class Bahridin_JavaArrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list=new ArrayList<>();
+
+        list.add("Dog");
+        list.add("Cat");
+        list.add("Bird");
+        list.add("Cow");
+
+        System.out.println("list = " + list);
+        System.out.println(list.get(0));
+        System.out.println(list.size());
+
+        ArrayList <Integer> list2=new ArrayList<>();
+
+        ArrayList <Double> list3=new ArrayList<>();
+
+        ArrayList <Boolean> list4=new ArrayList<>();
+
+
+
+    }
+}
