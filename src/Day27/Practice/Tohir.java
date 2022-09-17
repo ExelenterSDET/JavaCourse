8d6a8e578cbbf08a0747990c9049aec05ae85674
SaafarST <tohir.s91@gmail.com>
2022-09-17 05:45:12 -0700

    Day 27 homework

--- src/Day27/Practice/Tohir.java
+++ src/Day27/Practice/Tohir.java
@@ -1,7 +1,13 @@
 package Day27.Practice;
 
 public class Tohir {
+    public static void main(String[] args) {
 
+    }
+
+    static boolean stringsAreEqual(String s1, String s2){
+        return s1.toLowerCase().equals(s2.toLowerCase());
+    }
     void Returnoftotalnumberofstrings(String... str)
     {
 
