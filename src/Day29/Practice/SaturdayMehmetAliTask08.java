9c21980cb868d71ea4ac8f5c678efab751cb0672
maydinca <maydinca@gmail.com>
2022-09-18 16:21:31 -0400

    SatudayMehmetAliTasks

---
+++ src/Day29/Practice/SaturdayMehmetAliTask08.java
@@ -0,0 +1,33 @@
+package Day29.Practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SaturdayMehmetAliTask08 {
+    /*Task 7
+        Create a method that has an Arraylists of Integer as a parameter.
+        Remove all duplicates and return new ArrayList of Integer
+        Example:
+        [1, 3, 5, 4, 2, 3, 1, 2, 4, 6, 2, 5, 1]
+        output should be:
+        [1,2,3,4,5,6]
+ */
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1, 3, 5, 2, 2, 2, 7, 6, 7, 6, 7, 6, 7, 6, 7, 9, 2, 3, 1, 2, 6, 2, 5, 1));
+        SaturdayMehmetAliTask08 ut7 = new SaturdayMehmetAliTask08();
+
+        System.out.println(ut7.removeDub(arr));
+    }
+
+    ArrayList<Integer> removeDub(ArrayList<Integer> list) {
+        Collections.sort(list);
+        for (int i = list.size() - 1; i > 0; i--) {
+            if (list.get(i).equals(list.get(i - 1))) {
+                list.remove(i);
+            }
+        }
+//        System.out.println(list);
+        return list;
+    }
+}
