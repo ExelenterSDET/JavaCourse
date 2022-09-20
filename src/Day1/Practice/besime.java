f55f9869b0e0ce2e6c2ccf9a81aadf29139e1397
besime1 <bsmdmr86@gmail.com>
2022-09-20 14:50:29 -0400

    besime first commit

--- src/Day1/Practice/besime.java
+++
@@ -1,7 +0,0 @@
-package Day1.Practice;
-
-public class besime {
-    public static void main(String[] args) {
-        System.out.println("besime demir");
-    }
-}
---
+++ src/Day1/Practice/besime/Task1.java
@@ -0,0 +1,8 @@
+package Day1.Practice.besime;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Merhaba");
+
+    }
+}
