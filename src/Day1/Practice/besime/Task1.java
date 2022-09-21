4c59abff8b1df1eaeae694402f7bc3bf84c53c1c
besime1 <bsmdmr86@gmail.com>
2022-09-20 23:44:04 -0400

    fix cashed

--- src/Day1/Practice/besime/Task1.java
+++ src/Day1/Practice/besime/Task1.java
@@ -3,6 +3,7 @@ package Day1.Practice.besime;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println(" Merhaba");
 
     }
 }
