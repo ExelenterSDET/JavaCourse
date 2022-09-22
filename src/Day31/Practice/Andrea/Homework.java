e2eefafa2e349d9226a6e1ce4ab4e8ea6c207130
Andrea Mikulin <andreamikulin@gmail.com>
2022-09-21 20:16:08 -0600

    Not sure why it's making me commit again. Hopefully I am not making a big mistake.

--- src/Day31/Practice/Andrea/Homework.java
+++ src/Day31/Practice/Andrea/Homework.java
@@ -10,10 +10,6 @@ public class Homework {
     HashSet<String> chemistry = new HashSet<>(Arrays.asList("Yusuf", "Canan", "Demir", "Yigit", "Samil"));
     HashSet<String> physics = new HashSet<>(Arrays.asList("Demir", "Ibo", "Emir", "Canan", "Emel"));
     HashSet<String> math = new HashSet<>(Arrays.asList("Ronza", "Demir", "Kubra", "Emel", "Yigit", "Aizhan"));
-//        Task1. How many students we have in total.
-//        Task2. How many students are taking only Math
-//        Task3. How many students are taking both Chemistry and Physics
-//        Task4. How many students are taking all three classes.
 
         HashSet<String> total = new HashSet<>();
         total.addAll(chemistry);
