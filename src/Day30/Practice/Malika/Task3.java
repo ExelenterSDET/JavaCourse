073a60641a29989bea9dcc74278dd60172089e27
malikasadykova <Malika.sadykova@yahoo.com>
2022-09-24 01:27:54 +0300

    Created Task3

--- src/Day30/Practice/Malika/Task3.java
+++ src/Day30/Practice/Malika/Task3.java
@@ -2,7 +2,12 @@ package Day30.Practice.Malika;
 
 public class Task3 {
     public static void main(String[] args) {
-        
+
+        //TASK3
+ /*
+    Create a method, which will have list of list of Strings as a parameter.
+    Convert all Strings to be Camel Case. Example dog => Dog, if cat => Cat
+    return the list of list of strings*/
 
     }
 }
