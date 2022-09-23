342257fbd0801b2cdb5b6b539de9ee9af0b83d1a
malikasadykova <Malika.sadykova@yahoo.com>
2022-09-24 01:29:47 +0300

    Task3 solution

--- src/Day30/Practice/Malika/Task3.java
+++ src/Day30/Practice/Malika/Task3.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Task3 {
     public static void main(String[] args) {
 
-        //TASK3
+                    //TASK3
  /*
     Create a method, which will have list of list of Strings as a parameter.
     Convert all Strings to be Camel Case. Example dog => Dog, if cat => Cat
@@ -24,7 +24,6 @@ public class Task3 {
 
         ArrayList<ArrayList<String>> arrayLists = toCamelCase(listoflists);
         System.out.print(arrayLists + " ");
-
     }
 
     static ArrayList<ArrayList<String >> toCamelCase(ArrayList<ArrayList<String>> listofList){
