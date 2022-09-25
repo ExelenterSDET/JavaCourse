c962ecd3aeee19ae60788b54cf7172babac58480
malikasadykova <Malika.sadykova@yahoo.com>
2022-09-25 20:32:35 +0300

    Day30 Tasks Malika

--- src/Day30/Practice/Malika/Task3.java
+++ src/Day30/Practice/Malika/Task3.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Task3 {
     public static void main(String[] args) {
 
-        //TASK3
+                             //TASK3
  /*
     Create a method, which will have list of list of Strings as a parameter.
     Convert all Strings to be Camel Case. Example dog => Dog, if cat => Cat
