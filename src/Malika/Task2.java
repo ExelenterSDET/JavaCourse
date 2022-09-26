4c369363d14fe12a03f989beb78e6d567b9baa15
malikasadykova <Malika.sadykova@yahoo.com>
2022-09-27 00:17:40 +0300

    Day31 Tasks - Malika

--- src/Malika/Task2.java
+++ src/Malika/Task2.java
@@ -1,7 +1,31 @@
 package Malika;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Task2 {
     public static void main(String[] args) {
-        
+        //TASK2
+    /* Create a method with the parameter and return type of List of List of Character.
+    Method should return the characters in desc order
+    */
+        static void reverseList (ArrayList<ArrayList<Character>> list){
+            for (ArrayList<Character> characters : list) {
+                Collections.sort(characters);
+                Collections.reverse(characters);
+            }
+        }
+
+        public static void main(String[] args) {
+            ArrayList<ArrayList<Character>> list = new ArrayList<>();
+            list.add(new ArrayList<Character>(Arrays.asList('m','a','l','i','k','a')));
+            list.add(new ArrayList<Character>(Arrays.asList('a','l','i','h','a','n')));
+            list.add(new ArrayList<Character>(Arrays.asList('i','m','r','a','n')));
+
+            reverseList(list);
+            System.out.println(list);
+
+
     }
 }
