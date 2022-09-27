a0682bc1a7d6aab7fae6d907b11a7001b15e0217
Ufuk Sarac <saracufuk77@gmail.com>
2022-09-27 15:24:15 -0400

    day35 exercises

---
+++ src/Day35/Practice/Ufuktask3.java
@@ -0,0 +1,29 @@
+package Day35.Practice;
+
+import java.util.HashMap;
+import java.util.Map;
+/*
+Given an array of strings, return a Map<String, Boolean> where each different string is a key and its value is true if that string appears 2 or more times in the array.
+
+
+wordMultiple(["a", "b", "a", "c", "b"]) → {"a": true, "b": true, "c": false}
+wordMultiple(["c", "b", "a"]) → {"a": false, "b": false, "c": false}
+wordMultiple(["c", "c", "c", "c"]) → {"c": true}
+
+ */
+public class task3 {
+    public Map<String, Boolean> wordMultiple(String[] strings) {
+        HashMap<String,Boolean> map=new HashMap<>();
+        HashMap<String,Integer> map2=new HashMap<>();
+        for(String str:strings){
+            map2.put(str,map2.getOrDefault(str,0)+1);
+        }
+        for(String key:map2.keySet()){
+            if(map2.get(key)>1){
+                map.put(key,true);
+            }else map.put(key,false);
+        }
+        return map;
+    }
+
+}
---
+++ src/Day35/Practice/Ufuktask4.java
@@ -0,0 +1,30 @@
+package Day35.Practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/*
+Given a list of integers, return a list where each integer is added to 1 and the result is multiplied by 10.
+
+math1([1, 2, 3]) → [20, 30, 40]
+math1([6, 8, 6, 8, 1]) → [70, 90, 70, 90, 20]
+math1([10]) → [110]
+ */
+public class task4 {
+    public static void main(String[] args) {
+//        math1([10]);
+        List a=new ArrayList<>(Arrays.asList(1,2,3,5));
+
+        math1(a);
+    }
+    public static List<Integer> math1(List<Integer> nums) {
+        List<Integer> result= new ArrayList<>();
+        for (int i = 0; i < nums.size(); i++) {
+            result.add(i,(nums.get(i)+1)*10);
+        }
+        System.out.println("result = " + result);
+            return result;
+        }
+
+    }
+
