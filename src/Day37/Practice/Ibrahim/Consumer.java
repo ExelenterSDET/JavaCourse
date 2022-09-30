3d7299dd524461c2fdf84827b2184396b1e0ff5b
6-leaf <ibrahim6yaprak@gmail.com>
2022-09-30 18:50:39 -0400

    Ibrahim Day37

---
+++ src/Day37/Practice/Ibrahim/Consumer.java
@@ -0,0 +1,10 @@
+package Day37.Practice.Ibrahim;
+
+import java.util.ArrayList;
+
+public class Consumer {
+    String name;
+    double balanceDue;
+    Product product;
+    ArrayList<Product> cart;
+}
