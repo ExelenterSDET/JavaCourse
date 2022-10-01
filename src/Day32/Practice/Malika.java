726d3a5a0dd5eb16db5d8db6d65fff9a2531f79a
malikasadykova <Malika.sadykova@yahoo.com>
2022-10-01 23:56:16 +0300

    Task1

--- src/Day32/Practice/Malika.java
+++ src/Day32/Practice/Malika.java
@@ -1,7 +1,19 @@
 package Day32.Practice;
 
+import java.util.HashMap;
+
 public class Malika {
     public static void main(String[] args) {
-        
+        //TASK1
+        /* Create a map that holds an error status codes */
+
+        HashMap<Integer,String> errorCodes = new HashMap<>();
+        errorCodes.put(200, "OK");
+        errorCodes.put(201, "CREATED");
+        errorCodes.put(202, "ACCEPTED");
+        errorCodes.put(204, "NO CONTENT");
+        errorCodes.put(208, "ALREADY REPORTED");
+
+        System.out.print(errorCodes);
     }
 }
