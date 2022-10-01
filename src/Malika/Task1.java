f220aad936e0dbb624b1df4a24c19f98fa4b7271
malikasadykova <Malika.sadykova@yahoo.com>
2022-10-01 23:04:22 +0300

    Task1

--- src/Malika/Task1.java
+++ src/Malika/Task1.java
@@ -1,5 +1,20 @@
 package Malika;
 
+import java.util.HashMap;
+
 public class Task1 {
-    
+    public static void main(String[] args) {
+                        //TASK1
+        /* Create a map that holds an error status codes */
+
+        HashMap<Integer,String> errorCodes = new HashMap<>();
+        errorCodes.put(200, "OK");
+        errorCodes.put(201, "CREATED");
+        errorCodes.put(202, "ACCEPTED");
+        errorCodes.put(204, "NO CONTENT");
+        errorCodes.put(208, "ALREADY REPORTED");
+
+        System.out.print(errorCodes);
+    }
+
 }
