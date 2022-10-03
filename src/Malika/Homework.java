633b9f1827e37ba4b20af64b64f587585686a7c3
malikasadykova <Malika.sadykova@yahoo.com>
2022-10-04 00:04:00 +0300

    Homework

--- src/Malika/Homework.java
+++ src/Malika/Homework.java
@@ -1,7 +1,43 @@
 package Malika;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Homework {
     public static void main(String[] args) {
-        
+//TASK2
+        /*
+       Create a method, which will return a map with position as a key, and total compensation as value
+        Manager = 270000
+        Ceo = 525000
+        etc... */
+
+        HashMap<String, ArrayList<Integer>> compensation = new HashMap<>();
+        compensation.put("Manager", new ArrayList<>(Arrays.asList(100000, 120000, 50000)));
+        compensation.put("CEO", new ArrayList<>(Arrays.asList(300000, 150000, 75000)));
+        compensation.put("Admin", new ArrayList<>(Arrays.asList(70000, 90000, 30000)));
+        compensation.put("Accountant", new ArrayList<>(Arrays.asList(110000, 95000, 25000)));
+
+        HashMap<String, Integer> compensation1 = getComp(compensation);
+
+        System.out.println(compensation1);
+    }
+
+    static int getTotal (ArrayList<Integer> list){
+        int total = 0;
+        for (Integer integer : list) {
+            total += integer;
+        }
+        return total;
+    }
+
+    static HashMap<String, Integer> getComp (HashMap<String, ArrayList<Integer>> map){
+        HashMap<String, Integer> totalComp = new HashMap<>();
+        for (Map.Entry<String, ArrayList<Integer>> row: map.entrySet()) {
+            totalComp.put(row.getKey(), getTotal(row.getValue()));
+        }
+        return totalComp;
     }
 }
