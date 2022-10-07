657e911e2b38262973c36d6cd4b374a9904d512d
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-10-06 23:03:41 -0400

    Updates

--- src/Day43/Practice/Dilnoza/Name.java
+++ src/Day43/Practice/Dilnoza/Name.java
@@ -4,10 +4,19 @@ public class Name {
 
     public static void main(String[] args) {
 
+// 1. Method
+        for(String arg:args){
 
-        
-        String name="dilnoza";
-        System.out.println(name.substring(0,1).toUpperCase()+name.substring(1));
+            System.out.println(arg.substring(0,1).toUpperCase()+arg.substring(1));
+        }
+ //2. Method
+
+
+
+  // 3. Method Hard coding
+
+//        String name="dilnoza";
+//        System.out.println(name.substring(0,1).toUpperCase()+name.substring(1));
 
 
     }
