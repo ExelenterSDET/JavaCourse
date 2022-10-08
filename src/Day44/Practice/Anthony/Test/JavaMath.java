886d7055f4f9a7b1ebb4d45ab6f46aeaabd756e2
AnthonyHermann182 <herrmanthony2014@yahoo.com>
2022-10-08 09:27:00 -0700

    Day44AnthonyTasks

---
+++ src/Day44/Practice/Anthony/Test/JavaMath.java
@@ -0,0 +1,26 @@
+package Day44.Practice.Anthony.Test;
+
+import java.util.ArrayList;
+
+public class JavaMath {
+
+    public static int getTotal (int num){
+        int total =0;
+        for (int i = 1; i <= num; i++) {
+            total += i;
+
+        }
+        return total;
+    }
+    public static String getString(ArrayList<String> list, String firstLetter){
+        for(String str: list){
+            if(str.startsWith(firstLetter)){
+                return str;
+            }
+        }
+        return null;
+    }
+
+
+
+}
