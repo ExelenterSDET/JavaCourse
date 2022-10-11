88504561abdc56592ccee88f02920ad560c802ac
6-leaf <ibrahim6yaprak@gmail.com>
2022-10-11 12:49:05 -0400

    Ibrahim Saturday Project Task2

---
+++ src/Day44/Practice/Ibrahim/Saturday/Portfolio/CryptoMarket.java
@@ -0,0 +1,17 @@
+package Day44.Practice.Ibrahim.Saturday.Portfolio;
+
+public class CryptoMarket {
+
+    public static void main(String[] args) {
+        BuyCrypto buyCrypto = new BuyCrypto("BTC", 1.2);
+        BuyCrypto buyCrypto1 = new BuyCrypto("ETH", 1);
+        BuyCrypto buyCrypto2 = new BuyCrypto("BTC", 1.5);
+        BuyCrypto buyCrypto3 = new BuyCrypto("ETH", 1.5);
+        BuyCrypto buyCrypto4= new BuyCrypto("BTC", 2);
+        BuyCrypto buyCrypto5 = new BuyCrypto("ETH", 1.7);
+
+        
+
+        System.out.println(buyCrypto5);
+    }
+}
