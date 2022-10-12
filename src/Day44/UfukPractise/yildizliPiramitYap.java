429b30648396478a7d3f34ce597f2b12075c2bed
Ufuk Sarac <saracufuk77@gmail.com>
2022-10-12 16:56:11 -0400

    exercises for Java

---
+++ src/Day44/UfukPractise/yildizliPiramitYap.java
@@ -0,0 +1,30 @@
+package Test1;
+
+public class yildizliPiramitYap {
+    public static void main(String[] args) {
+
+        //duz piramit
+        int rows=5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 0; j <= rows-i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j < i+1; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println("");
+
+        }
+
+        //ters piramit
+        for (int i = rows-1; i >=1; i--) {
+            for (int j = 0; j <= rows-i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j < i+1; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
