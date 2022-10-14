12873b4196ddaf9675320ebf6911451647617082
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-10-13 22:54:24 -0400

    Homework

---
+++ src/Day47/Practice/Dilnoza/HomeWork/Homework1.java
@@ -0,0 +1,13 @@
+package Day47.Practice.Dilnoza.HomeWork;
+
+public class Homework1 {
+   /*
+    Task3.
+    Update Saturday Project task 2 using Enum CryptoCurrency
+    add class to saturday project and replace bitcoin/etherium with enum
+    short name;
+    final price
+    */
+
+
+}
