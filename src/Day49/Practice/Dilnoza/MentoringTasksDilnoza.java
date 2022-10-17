1c5613eb54b3823d34e3611391145d9b9e9afe9f
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-10-16 22:50:37 -0400

    mentoringTask

--- src/Day49/Practice/Dilnoza/MentoringTasksDilnoza.java
+++ src/Day49/Practice/Dilnoza/MentoringTasksDilnoza.java
@@ -1,6 +1,7 @@
 package Day49.Practice.Dilnoza;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -11,17 +12,32 @@ public class MentoringTasksDilnoza {
         return list and count of numbers divisible by 8;
  */
     public static void main(String[] args) {
-
-
         Random random = new Random();
-        List<Integer> newList= new ArrayList<>();
+      int num= random.nextInt(100);
+        for (int i = 0; i < num ; i++) {
+            if(i%8==0){
+                System.out.print(i+", ");
+            }
+
+        }
+
+
+
+
+        /*
+         List<Integer> newList= new ArrayList<>();
         newList.add(random.nextInt(100));
         int count=0;
         for (int i = 0; i < newList.size(); i++) {
             if (newList.get(i)%8==0){
                 count+=newList.get(i);
+                System.out.println( i+", ");
             }
 
         }
+        System.out.println("count = " + count);
+         */
+
+
     }
 }
