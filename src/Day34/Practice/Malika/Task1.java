a984bfe5c3c797f19eb9524c1143e1d9df0bfed4
malikasadykova <Malika.sadykova@yahoo.com>
2022-10-18 00:34:30 +0300

    created task1 class

--- src/Day34/Practice/Malika/Task1.java
+++ src/Day34/Practice/Malika/Task1.java
@@ -1,5 +1,21 @@
 package Day34.Practice.Malika;
 
 public class Task1 {
-    
+ /*
+    Task1
+
+      Create a method to print all the keys and values
+      using entry set and key set approaches with forEach method.
+
+   Task 2
+
+       if key = a, add to value 10;
+       if key = b, add to value 20;
+       if key = c, add to value 30;
+       if key = d, add to value 40;
+       if key = e, add to value 50;
+       if key = f, add to value 60;
+       if key = g, add to value 70;
+
+    */
 }
