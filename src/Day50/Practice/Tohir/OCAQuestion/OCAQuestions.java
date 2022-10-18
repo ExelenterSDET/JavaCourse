e94d27bcc6f0759ec50825c3f16d0ea1fc076f05
SaafarST <tohir.s91@gmail.com>
2022-10-17 19:39:50 -0700

    Day50

---
+++ src/Day50/Practice/Tohir/OCAQuestion/OCAQuestions.java
@@ -0,0 +1,16 @@
+package Day50.OCAQuestion;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Calendar;//Under util package
+
+public class OCAQuestions {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2020, Month.AUGUST,3);
+        date.plusDays(40);
+        date.plusMonths(3);
+        System.out.println(date.getYear()+" "+date.getMonth()+" "+date.getDayOfMonth());
+        LocalDate date1 = LocalDate.of(3020, Calendar.APRIL,4);
+        System.out.println(date1);
+    }
+}
