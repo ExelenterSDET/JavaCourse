4c78fae8ca6941c6025d99a435265b55d849eb50
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-10-18 22:44:31 -0400

    Tasks

---
+++ src/Day51/Practice/Dilnoza/Questions.java
@@ -0,0 +1,12 @@
+package Day51.Practice.Dilnoza;
+
+public class Questions {
+    public static void main(String[] args) {
+
+
+   Object obj = new Integer(3);
+    String str = (String ) obj;
+        System.out.println("str = " + str);
+        System.out.println(1/0);
+}
+   }
