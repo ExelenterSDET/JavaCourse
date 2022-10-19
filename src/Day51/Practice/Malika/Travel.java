e10d666d7a800bef0eb4730f8dc15cf0996841bc
malikasadykova <Malika.sadykova@yahoo.com>
2022-10-20 02:59:40 +0300

    added Travel class

--- src/Day51/Practice/Malika/Travel.java
+++ src/Day51/Practice/Malika/Travel.java
@@ -1,4 +1,37 @@
 package Day51.Practice.Malika;
 
 public class Travel {
+    public static void main(String[] args) {
+
+        try { travelToOcean(HOT);
+
+        } catch (RuntimeException ex){
+            System.out.println(ex.getMessage());
+
+        }
+
+        try{ travelToMountain(Weather.COLD);
+
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+    public static void travelToOcean (Weather weather){
+        if (weather == HOT) {
+            System.out.println("You may travel to the ocean");
+        } else {
+            throw new RuntimeException("You many not travel to the ocean");
+        }
+    }
+
+    public static void travelToMountain (Weather weather) throws InterruptedException {
+        if (weather == WARM) {
+            System.out.println("You may travel to mountain ");
+        } else {
+            throw new InterruptedException("You may not travel to mountain ");
+        }
+
+    }
+
+}
 }
