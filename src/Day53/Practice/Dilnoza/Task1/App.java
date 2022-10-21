dd6e6627b3fcca5b603d5dde257cbccdf4bb19c3
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-10-20 22:33:14 -0400

    Strawberry Class

---
+++ src/Day53/Practice/Dilnoza/Task1/App.java
@@ -0,0 +1,37 @@
+package Day53.Practice.Dilnoza.Task1;
+
+public class App {
+    public static void main(String[] args) {
+
+//        Fruit fruit = new Fruit();
+//        fruit.setName("Fruit");
+//        fruit.setPrice(200);
+//        fruit.setColor("Any color");
+//
+//        System.out.println(fruit);
+//
+//        Apple apple = new Apple();
+//        apple.setName("Red Apple");
+//        apple.setColor("RED");
+//        apple.setDiscount(15);
+//        apple.setPrice(100); // 100 - 100*0/100
+//        System.out.println(apple);
+//
+//        Banana banana= new Banana();
+//        banana.setStoreAddress("11233 White St");
+//        banana.setName("Baby Banana");
+//        banana.setPrice(1.99);
+//        banana.setColor("Yellow");
+//        System.out.println("Banana = " + banana);
+//
+//
+//
+//        Strawberry strawberry= new Strawberry();
+//        strawberry.setBrand("Driscoll");
+//        strawberry.setOwner("Store");
+//        strawberry.setName("White Strawberry");
+//        strawberry.setColor("White");
+//        strawberry.setPrice(5.99);
+//        System.out.println("Strawberry = " + strawberry);
+   }
+}
