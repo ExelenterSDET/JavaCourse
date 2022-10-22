6d5a26b2cb033d6aea0de4ff4bad9a186abda878
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-10-21 22:15:24 -0400

    Test

--- src/Day53/Practice/Dilnoza/Empty.java
+++ src/Day53/Practice/Dilnoza/Empty.java
@@ -3,5 +3,6 @@ package Day53.Practice.Dilnoza;
 public class Empty {
     public static void main(String[] args) {
         System.out.println("Empty");
+        System.out.println("Another one");
     }
 }
