7b9555c7e3b838e212122b692f8140f9efb6fd39
malikasadykova <Malika.sadykova@yahoo.com>
2022-10-25 02:23:48 +0300

    created Task1

--- src/Day53/Practice/Malika/Task1/Apple.java
+++ src/Day53/Practice/Malika/Task1/Apple.java
@@ -1,5 +1,18 @@
 package Day53.Practice.Malika.Task1;
 
-public class Apple {
-    
-}
+
+
+    public class Apple extends Fruit {
+        int discount; // 15
+
+        public void setDiscount(int discount) {
+            this.discount = discount;
+        }
+
+        @Override
+        public void setPrice(double price) {
+            super.price = price - (price * discount / 100);
+        }
+    }
+
+
