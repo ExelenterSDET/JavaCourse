a104ead9fc232a4ff930da4bf13d3a103963d9f4
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-10-24 22:44:11 -0400

    Book Class

---
+++ src/Day55/Practice/Dilnoza/Book.java
@@ -0,0 +1,34 @@
+package Day55.Practice.Dilnoza;
+
+public class Book {
+    private String name;
+    private String author;
+    private int year;
+
+    public Book(String name, String author, int year) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ src/Day55/Practice/Dilnoza/Library.java
@@ -0,0 +1,7 @@
+package Day55.Practice.Dilnoza;
+
+public class Library {
+
+
+
+}
