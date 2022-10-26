971898b6af138f2572fe0736454958573b58d11f
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2022-10-25 22:12:14 -0400

    Test

--- src/Day56/Practice/Dilnoza/Test.java
+++ src/Day56/Practice/Dilnoza/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Empty Test");
         System.out.println("Test");
+        System.out.println("test");
     }
 }
