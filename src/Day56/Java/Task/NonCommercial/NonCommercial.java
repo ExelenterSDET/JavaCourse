543da45510224cf52c5a73aee85e737bfe57ed53
Furkat-Ali-Khalilov <khalilov.furkat.ali@gmail.com>
2022-10-25 22:08:52 -0400

    Day56

---
+++ src/Day56/Java/Task/NonCommercial/NonCommercial.java
@@ -0,0 +1,26 @@
+package Day56.Java.Task.NonCommercial;
+
+import Day56.Java.Task.Vehicle.Vehicle;
+
+public abstract class NonCommercial extends Vehicle {
+    protected String plateNumber;
+    protected double insurancePrice;
+
+    public NonCommercial(String brand, String model, int speed) {
+        super(brand, model, speed);
+    }
+
+    public NonCommercial(String brand, String model, int speed, String plateNumber, double insurancePrice) {
+        super(brand, model, speed);
+        this.plateNumber = plateNumber;
+        this.insurancePrice = insurancePrice;
+    }
+
+    public abstract String getPlateNumber();
+
+    public abstract void setPlateNumber(String plateNumber);
+
+    public abstract double getInsurancePrice();
+
+    public abstract void setInsurancePrice(double insurancePrice);
+}
